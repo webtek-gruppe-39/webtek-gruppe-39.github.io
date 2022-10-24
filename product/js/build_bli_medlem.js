1f30c9eecde9e383b3314cf38674a95f40361d34
badallmann <badallmann@gmail.com>
2022-10-24 18:00:33 +0200

    ordne ordne

---
+++ product/js/build_bli_medlem.js
@@ -0,0 +1,15 @@
+import { H, Z, lorem } from './mini-lib.js'
+
+// build main
+console.log('Bli medlem!')
+
+// test
+const a = H.div().textContent = 'heia'
+document.querySelector('main').append(a)
+
+
+
+
+
+
+// input field .focus()
