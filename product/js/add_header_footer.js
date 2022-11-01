5fb854a6943ffdabe52080e4f4c833799afa6dba
badallmann <badallmann@gmail.com>
2022-11-01 13:21:20 +0100

    add header footer

---
+++ product/js/add_header_footer.js
@@ -0,0 +1,16 @@
+// spytter inn header og footer fra et dokument som fungerer som templat
+
+// skaff siden som et dokument
+const xmlhttp = new XMLHttpRequest()
+xmlhttp.open("GET", 'header_footer.html', false)
+xmlhttp.send()
+const template = document.createElement('html')
+template.innerHTML = xmlhttp.responseText
+
+// isolér header og footer
+const header = template.querySelector('header')
+const footer = template.querySelector('footer')
+
+// spytt inn
+document.body.prepend(header)
+document.body.append(footer)
