6b6bf60f6ef946bf2d33c21669778ce75e4491e6
HaakonWiland <114581743+HaakonWiland@users.noreply.github.com>
2022-11-07 09:28:02 +0100

    Lagt inn alle kart

--- product/js/fiskekart.js
+++ product/js/fiskekart.js
@@ -1,8 +1,46 @@
 
-function display_map () {
-    document.querySelector("#fiskekart").style.display = "block";
+
+// Map for Torsk:
+function display_torsk_map () {
+    document.querySelector("#torsk_kart").style.display = "block";
 }
 
-function hide_map () {
-    document.querySelector("#fiskekart").style.display = "none";
+function hide_map_tosk_map () {
+    document.querySelector("#torsk_kart").style.display = "none";
+}
+
+// Map for Sei:
+function display_sei_map () {
+    document.querySelector("#sei_kart").style.display = "block";
+}
+
+function hide_map_sei_map () {
+    document.querySelector("#sei_kart").style.display = "none";
+}
+
+// Map for Makrell:
+function display_makrell_map () {
+    document.querySelector("#makrell_kart").style.display = "block";
+}
+
+function hide_makrell_map () {
+    document.querySelector("#makrell_kart").style.display = "none";
+}
+
+// Map for Ørret:
+function display_orret_map () {
+    document.querySelector("#orret_kart").style.display = "block";
+}
+
+function hide_orret_map () {
+    document.querySelector("#orret_kart").style.display = "none";
+}
+
+// Map for Kveite:
+function display_kveite_map () {
+    document.querySelector("#kveite_kart").style.display = "block";
+}
+
+function hide_kveite_map () {
+    document.querySelector("#kveite_kart").style.display = "none";
 }
