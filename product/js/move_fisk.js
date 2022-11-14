6b5a75b78fdf6721cfc6c26ed1e3753f3910417b
ingvieo <ingvild.engen.okland@hotmail.com>
2022-11-14 22:28:15 +0100

    fiskehopping

---
+++ product/js/move_fisk.js
@@ -0,0 +1,44 @@
+ document.onmousemove = moveAway;
+
+ function moveAway(evnt){
+
+    if (!evnt){
+        evnt = window.event;
+    }
+
+    var x = evnt.clientX;
+    var y = evnt.clientY;
+
+    xProsent = x / window.innerWidth * 65;
+    yProsent = y / window.innerHeight * 70;
+
+    var posisjon = document.getElementById("logoindex").style;
+
+    // document.getElementById("x").innerHTML = xProsent;
+    // document.getElementById("y").innerHTML = yProsent;
+
+  
+    posisjon.left = yProsent + "%";
+    posisjon.top = xProsent + "%";
+  
+ }
+
+
+
+//document.onmousemove = flyttFisk;
+
+
+// function flyttFisk(m){
+
+//     if(!m){
+//         m=window.event;
+//     }
+//     var x = m.clientX;
+//     var y = m.clientY;
+
+//     var endring = document.getElementById("logoindex").style;
+
+//     endring.left = y + "%";
+//     endring.top = x + "%";
+// }
+  
